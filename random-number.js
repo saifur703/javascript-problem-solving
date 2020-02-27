4b8701bebf96d24ce70837d513ddba4cf32ac756
saifur <saifur703@gmail.com>
2020-02-27 10:08:02 +0600

    ludu chokka random number

---
+++ random-number.js
@@ -0,0 +1,32 @@
+/**
+ * Practice
+ *
+ * ceil function
+ * flor function
+ * round function
+ * random function
+ */
+var num1 = 44.5;
+var ceil = Math.ceil(num1);
+console.log('Ceil Number: ', ceil);
+
+var num2 = 11.879;
+var floor = Math.floor(num2);
+console.log('Floor Number: ', floor);
+
+var num3 = 31.826;
+var round = Math.round(num3);
+console.log('Round Number: ', round);
+
+var num4 = 10;
+var random = Math.random();
+console.log('Random Number: ', random);
+
+/**
+ * Ludu Chokkha
+ */
+
+var ludu = 6;
+var rand = Math.random() * ludu;
+var chokka = Math.round(rand);
+console.log('Ludu Chokka: ', chokka);
